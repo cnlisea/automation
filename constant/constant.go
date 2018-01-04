8768d5de4be71d24c7df43ccd35e6f820d0129e5
lisea <lihai@jubao56.com>
2018-01-04 20:42:46 +0800

    add constant files

---
+++ constant/constant.go
@@ -0,0 +1,38 @@
+package constant
+
+const (
+	ConfigRequestUrl    = "url"
+	ConfigRequestMethod = "method"
+	ConfigRequest       = "request"
+	ConfigResponse      = "response"
+)
+
+const (
+	RequestHttpUrl         = "request_url"
+	RequestHttpMethod      = "request_method"
+	RequestHttpBody        = "request_body"
+	RequestHttpBodyType    = "request_body_type"
+	RequestHttpAuthType    = "request_auth_type"
+	RequestHttpAuthToken   = "http_request_auth_token"
+	ResponseHttpStatusCode = "http_response_status_code"
+)
+
+// doc
+const (
+	Author   = "author"   // 作者
+	Title    = "title"    // 标题
+	Function = "function" // 功能
+	Explain  = "explain"  // 说明，一条或者多条
+	Auth     = "auth"     // 认证
+)
+
+//
+const (
+	DescSuffix = "_d"
+	TypeSuffix = "_t"
+)
+
+// default
+const (
+	DefaultAuthor = "root"
+)
