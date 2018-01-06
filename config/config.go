9caec9de490878fade7ba561e7dd56b85d7a62d4
lisea <lihai@jubao56.com>
2018-01-06 15:05:25 +0800

    remove ParseConfigFile method

--- config/config.go
+++ config/config.go
@@ -10,167 +10,16 @@ import (
 )
 
 var (
-	BaseUrl = "http://test.jubao56.com:5000"
+	BaseUrl = ""
 	/*
 		* 0 No Auth
 			* 1 Bearer Token
 	*/
-	AuthType     = http.BearerTokenAuth
+	AuthType     = http.NoAuth
 	TokenContain = "data"
 	TokenKey     = "access_token"
 	Token        = ""
 )
-
-var (
-	GlobalConfig = []interface{}{}
-)
-
-func ParseConfigFile(path string) ([]interface{}, error) {
-	// read all
-	b, err := utils.ReadFileAll(path)
-	if nil != err {
-		return nil, err
-	}
-
-	var config []interface{}
-
-	data := make(map[string]interface{})
-	if err = json.Unmarshal(b, &data); nil != err {
-		return nil, err
-	}
-
-	if 0 == len(data) {
-		return []interface{}{}, nil
-	}
-
-	for k, v := range data {
-		switch strings.ToLower(k) {
-		case "baseurl":
-			fallthrough
-		case "base_url":
-			fallthrough
-		case "url":
-			BaseUrl = utils.ToString(v)
-		case "authtype":
-			fallthrough
-		case "auth_type":
-			fallthrough
-		case "auth":
-			authType, err := strconv.Atoi(utils.ToString(v))
-			if nil != err {
-				return nil, err
-			}
-			AuthType = authType
-		case "token_contain":
-			fallthrough
-		case "tokencontain":
-			TokenContain = utils.ToString(v)
-		case "tokenkey":
-			fallthrough
-		case "token_key":
-			TokenKey = utils.ToString(v)
-		case "token":
-			Token = utils.ToString(v)
-		default:
-			cData, ok := v.(map[string]interface{})
-			if !ok {
-				continue
-			}
-
-			if !strings.Contains(k, "_") {
-				for kk, vv := range cData {
-					ccData, ok := vv.(map[string]interface{})
-					if !ok {
-						continue
-					}
-
-					var req, res interface{}
-					if val, ok := ccData["req"]; ok {
-						req = val
-					}
-
-					if val, ok := ccData["request"]; ok {
-						req = val
-					}
-
-					if val, ok := ccData["res"]; ok {
-						res = val
-					}
-
-					if val, ok := ccData["response"]; ok {
-						res = val
-					}
-
-					if nil == req || nil == res {
-						//TODO logs req or res is nil
-						continue
-					}
-
-					req, res = RecoverType(req), RecoverType(res)
-					/*fmt.Println("req:")
-					for k, v := range req.(map[string]interface{}){
-						t := reflect.TypeOf(v)
-						fmt.Println(k, " ", t.String())
-					}
-					fmt.Println("res:")
-					for k, v := range res.(map[string]interface{}){
-						t := reflect.TypeOf(v)
-						fmt.Println(k, " ", t.String())
-					}*/
-					config = append(config, k+"_"+kk, req, res)
-				}
-			} else {
-				var req, res interface{}
-				if val, ok := cData["req"]; ok {
-					req = val
-				}
-
-				if val, ok := cData["request"]; ok {
-					req = val
-				}
-
-				if val, ok := cData["res"]; ok {
-					res = val
-				}
-
-				if val, ok := cData["response"]; ok {
-					res = val
-				}
-
-				if nil == req || nil == res {
-					//TODO logs req or res is nil
-					continue
-				}
-
-				if v, ok := req.(map[string]interface{}); ok {
-					_ = v
-				}
-
-				if v, ok := res.(map[string]interface{}); ok {
-					_ = v
-				}
-
-				req, res = RecoverType(req), RecoverType(res)
-				/*fmt.Println("req:")
-				for k, v := range req.(map[string]interface{}){
-					t := reflect.TypeOf(v)
-					fmt.Println(k, " ", t.String())
-				}
-				fmt.Println("res:")
-				for k, v := range res.(map[string]interface{}){
-					t := reflect.TypeOf(v)
-					fmt.Println(k, " ", t.String())
-				}*/
-				config = append(config, k, req, res)
-			}
-		}
-	}
-
-	// save global config
-	GlobalConfig = config
-
-	return config, nil
-}
 
 func RecoverType(data interface{}) interface{} {
 	switch data.(type) {
